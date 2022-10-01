86e5cc16f1ac428d2817cac28113208f70b48149
Jonatan Primera <jonatanprimera84@gmail.com>
2022-10-01 16:16:44 -0500

    tema3

---
+++ tema3/factorial_break.js
@@ -0,0 +1,8 @@
+
+for (let i = 0; i < 10; i++) {
+    res = i
+    console.log(res)
+    if (i === 5){
+        break;
+    }
+}
---
+++ tema3/factorial_wile.js
@@ -0,0 +1,7 @@
+contador = 0;
+
+while (contador < 10) {
+    res = contador * 10;
+    contador++
+    console.log(res)
+}
