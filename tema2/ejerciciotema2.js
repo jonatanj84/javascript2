853555b0af0d046d6e7144c2aafa994c30862449
Jonatan Primera <jonatanprimera84@gmail.com>
2022-10-01 11:54:48 -0500

    tema 1-2

---
+++ tema2/ejerciciotema2.js
@@ -0,0 +1,16 @@
+let nombre = "Jonatan Primera";
+
+let edad = 38;
+
+let desarrollador = true;
+
+const fechaDeNacimiento = new Date (1984, 6, 26);
+
+const libroFavorito = {
+    titulo: "La culpa es de la vaca",
+    autor: "Jaime Lopera Gutierrez",
+    fecha:"agosto 2003",
+    url: "https://es.pdfdrive.com/la-culpa-es-de-la-vaca-i-e39439674.html",
+}
+
+
