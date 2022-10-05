414a6e2efec6cff7d40be801bfe2f7e02c6f951c
Jonatan Primera <jonatanprimera84@gmail.com>
2022-10-04 19:47:17 -0500

    numeros

---
+++ tema5_numeros/ejercicio5_numeros.js
@@ -0,0 +1,10 @@
+let alturaCentimetor = 1800;
+let alturaMetros = 1.80;
+let pesoKilogramos = 65.400;
+let alturaMetrosRedondeada = alturaMetros.toFixed(0);
+let pesoKilogramosRedondeado = pesoKilogramos.toPrecision(2);
+let max_valor_js = Number.MAX_VALUE + 1;
+
+console.log(alturaMetrosRedondeada);
+console.log(pesoKilogramosRedondeado);
+console.log(max_valor_js)
