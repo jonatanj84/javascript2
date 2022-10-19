62ca00278829efe862634b6d049d6431a626223a
Jonatan Primera <jonatanprimera84@gmail.com>
2022-10-19 17:08:13 -0500

    eventos

---
+++ tema14_eventos/eventos/index.js
@@ -0,0 +1,11 @@
+
+let boton = document.getElementById("btn")
+
+
+boton.addEventListener("click", () => {
+    alert("click en el botón")
+})
+
+$("#btn").click(() =>{
+    console.log("Hola, estoy utilizando jQuery")
+})
