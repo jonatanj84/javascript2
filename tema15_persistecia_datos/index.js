ebff40602764a68ce39c45a7215504d49e8a6e6f
Jonatan Primera <jonatanprimera84@gmail.com>
2022-10-20 18:43:52 -0500

    persistencia de datos

---
+++ tema15_persistecia_datos/index.js
@@ -0,0 +1,12 @@
+let nombre = "Jonatan";
+let apellido = "Primera";
+const persona = {
+    nombre: nombre,
+    apellido: apellido
+}
+
+/*localStorage.setItem("persona",JSON.stringify(persona));
+
+sessionStorage.setItem("persona",JSON.stringify(persona));
+
+document.cookie = "nombre = Jonatan Primera; expire=" + new Date(2022,9,20,18,45,00).toUTCString*/
